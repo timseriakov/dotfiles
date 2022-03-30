a69778c380d0b0fcdcd75e613ce93137f792d73f
Tim Seriakov <t.seriakov@gmail.com>
2022-03-31 00:12:06 +0300

    add something

--- config.lua
+++ config.lua
@@ -11,13 +11,27 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
--- lvim.colorscheme = "dracula"
 lvim.colorscheme = "nord"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.format_on_save = true
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { exe = "flake8" },
+  {
+    exe = "shellcheck",
+    args = { "--severity", "warning" },
+  },
+  {
+    exe = "codespell",
+    filetypes = { "javascript", "python", "lua" },
+  },
+}
+
 -- unmap a default keymapping
 -- cvim.keys.normal_mode["<C-Up>"] = false
 -- edit a default keymapping
@@ -42,20 +56,27 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
--- }
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
+}
+lvim.builtin.which_key.mappings["S"]= {
+  name = "Session",
+  c = { "<cmd>lua require('persistence').load()<cr>", "Restore last session for current dir" },
+  l = { "<cmd>lua require('persistence').load({ last = true })<cr>", "Restore last session" },
+  Q = { "<cmd>lua require('persistence').stop()<cr>", "Quit without saving session" },
+}
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+-- lvim.builtin.dashboard.active = true
+ -- lvim.builtin.alpha.mode = "dashbaord" -- or "startify"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
@@ -143,17 +164,143 @@ lvim.plugins = {
     {"antoinemadec/FixCursorHold.nvim"},
     {"shaunsingh/nord.nvim"},
     {"junegunn/goyo.vim"},
-    {"booperlv/nvim-gomove"},
     {"justinmk/vim-sneak"},
     {"lukas-reineke/indent-blankline.nvim"},
+    {"wakatime/vim-wakatime"},
     {
       "folke/trouble.nvim",
       cmd = "TroubleToggle",
     },
+    -- {
+    --   "pwntester/octo.nvim",
+    --   event = "BufRead",
+    -- },
+    {
+      "nacro90/numb.nvim",
+      event = "BufRead",
+      config = function()
+      require("numb").setup {
+        show_numbers = true, -- Enable 'number' for the window while peeking
+        show_cursorline = true, -- Enable 'cursorline' for the window while peeking
+      }
+      end,
+    },
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      event = "BufRead",
+      setup = function()
+        vim.g.indentLine_enabled = 1
+        vim.g.indent_blankline_char = "вЦП"
+        vim.g.indent_blankline_filetype_exclude = {"help", "terminal", "dashboard", "alpha"}
+        vim.g.indent_blankline_buftype_exclude = {"terminal"}
+        vim.g.indent_blankline_show_trailing_blankline_indent = false
+        vim.g.indent_blankline_show_first_indent_level = false
+      end
+    },
+    {
+      "windwp/nvim-ts-autotag",
+      event = "InsertEnter",
+      config = function()
+        require("nvim-ts-autotag").setup()
+      end,
+    },
+    {"p00f/nvim-ts-rainbow"},
+    {
+      "nvim-telescope/telescope-project.nvim",
+      event = "BufWinEnter",
+      setup = function()
+      vim.cmd [[packadd telescope.nvim]]
+      end,
+    },
+    {
+      "rmagatti/goto-preview",
+      config = function()
+      require('goto-preview').setup {
+          width = 120; -- Width of the floating window
+          height = 25; -- Height of the floating window
+          default_mappings = true; -- Bind default mappings
+          debug = false; -- Print debug information
+          opacity = nil; -- 0-100 opacity level of the floating window where 100 is fully transparent.
+          post_open_hook = nil -- A function taking two arguments, a buffer and a window to be ran as a hook.
+          -- You can use "default_mappings = true" setup option
+          -- Or explicitly set keybindings
+          -- vim.cmd("nnoremap gpd <cmd>lua require('goto-preview').goto_preview_definition()<CR>")
+          -- vim.cmd("nnoremap gpi <cmd>lua require('goto-preview').goto_preview_implementation()<CR>")
+          -- vim.cmd("nnoremap gP <cmd>lua require('goto-preview').close_all_win()<CR>")
+      }
+      end
+    },
+    {
+      "ray-x/lsp_signature.nvim",
+      event = "BufRead",
+      config = function()
+        require "lsp_signature".setup()
+      end
+    },
+    { --- TODO добавить хоткей
+      "simrat39/symbols-outline.nvim",
+      cmd = "SymbolsOutline",
+    },
+    {
+      "metakirby5/codi.vim",
+      cmd = "Codi",
+    },
+  	{
+      "ethanholz/nvim-lastplace",
+      event = "BufRead",
+      config = function()
+        require("nvim-lastplace").setup({
+          lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+          lastplace_ignore_filetype = {
+            "gitcommit", "gitrebase", "svn", "hgcommit",
+          },
+          lastplace_open_folds = true,
+        })
+      end,
+    },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    module = "persistence",
+    config = function()
+      require("persistence").setup {
+        dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
+        options = { "buffers", "curdir", "tabpages", "winsize" },
+      }
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+    require("todo-comments").setup()
+    end,
+  },
+  {
+    "itchyny/vim-cursorword",
+    event = {"BufEnter", "BufNewFile"},
+    config = function()
+      vim.api.nvim_command("augroup user_plugin_cursorword")
+      vim.api.nvim_command("autocmd!")
+      vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
+      vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
+      vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
+      vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
+      vim.api.nvim_command("augroup END")
+      end
+  },
+  {
+    "tpope/vim-surround",
+    keys = {"c", "d", "y"}
+  },
+  {
+    "npxbr/glow.nvim",
+    ft = {"markdown"},
+  },
 }
 
 require("better_escape").setup {
-    mapping = {"jk", "jj", "df"}, -- a table with mappings to use
+    mapping = {"jk", "jj"}, -- a table with mappings to use
     timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
     clear_empty_lines = false, -- clear line after escaping if there is only whitespace
     keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
@@ -161,20 +308,6 @@ require("better_escape").setup {
     -- keys = function()
     --   return vim.fn.col '.' - 2 >= 1 and '<esc>l' or '<esc>'
     -- end,
-}
-
-require("gomove").setup {  -- whether or not to map default key bindings, (true/false)
-  map_defaults = true,  -- whether or not to reindent lines moved vertically (true/false)
-  reindent = true,  -- whether or not to undojoin same direction moves (true/false)
-  undojoin = true,  -- whether to not to move past end column when moving blocks horizontally, (true/false)
-  move_past_end_col = false,  -- whether or not to ignore indent when duplicating lines horizontally, (true/false)
-  ignore_indent_lh_dup = true,
-}
-
-require("indent_blankline").setup {
-    -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
-    show_current_context_start = true,
 }
 
 vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
