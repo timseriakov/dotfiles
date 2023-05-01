15ce126d5eaf9446a6bf28c267041f9544f86748
Tim Seriakov <t.seriakov@gmail.com>
2023-05-01 20:14:17 +0400

    add something

--- lvim/config.lua
+++ lvim/config.lua
@@ -215,18 +215,18 @@ lvim.plugins = {
       -- vim.api.nvim_set_keymap("n", "-", ":RnvimrToggle<CR>", { noremap = true, silent = true })
     end,
   },
-  -- {
-  --   "norcalli/nvim-colorizer.lua",
-  --   config = function()
-  --     require("colorizer").setup({ "css", "scss", "html", "javascript", "cjs", "jsx", "tsx" }, {
-  --       RGB = true,      -- #RGB hex codes
-  --       RRGGBB = true,   -- #RRGGBB hex codes
-  --       RRGGBBAA = true, -- #RRGGBBAA hex codes
-  --       rgb_fn = true,   -- CSS rgb() and rgba() functions
-  --       hsl_fn = true,   -- CSS hsl() and hsla() functions
-  --       css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-  --       css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
-  --     })
-  --   end,
-  -- },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "css", "scss", "html", "javascript", "cjs", "jsx", "tsx" }, {
+        RGB = true,      -- #RGB hex codes
+        RRGGBB = true,   -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true,   -- CSS rgb() and rgba() functions
+        hsl_fn = true,   -- CSS hsl() and hsla() functions
+        css = true,      -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true,   -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
+    end,
+  },
 }
