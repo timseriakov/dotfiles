f36ba179a922437d94c0cd2f6a5f8a83bd22e3a9
Tim Seriakov <t.seriakov@gmail.com>
2023-07-07 21:22:30 +0400

    add something

--- fish/config.fish
+++ fish/config.fish
@@ -7,18 +7,17 @@ end
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-set -g -x fish_greeting 'Welcome to fish'
-
-fish_vi_key_bindings
+set -g -x fish_greeting 'Welcome'
 
 starship init fish | source
 thefuck --alias | source
 
+fish_vi_key_bindings
+set fish_key_bindings fish_user_key_bindings
+
 set --universal --export fzf_preview_file_cmd 'bat --style changes --theme Nord --color=always'
 set --universal --export fzf_fd_opts --hidden --exclude=.git
 set --universal --export FZF_DEFAULT_OPTS --color fg:#5E81AC,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#2E3440,hl+:#A3BE8C --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker='*'
-
-set fish_key_bindings fish_user_key_bindings
 
 # pnpm
 set -gx PNPM_HOME "/Users/tim/Library/pnpm"
@@ -31,8 +30,9 @@ test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.
 
 pyenv init - | source
 set -x PATH /platform-tools $PATH;
+set -gx fish_command_timeout 8000
 
 test -d "$HOME/.tea" && "$HOME/.tea/tea.xyz/v*/bin/tea" --magic=fish --silent | source
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-set -gx fish_command_timeout 5000
+
+# set -gx VOLTA_HOME "$HOME/.volta"
+# set -gx PATH "$VOLTA_HOME/bin" $PATH
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,9 @@
 function fish_user_key_bindings
   fish_vi_key_bindings
+  for mode in insert default visual
+      bind -M $mode \ck 'history --merge ; up-or-search'
+      bind -M $mode \cj 'history --merge ; down-or-search' 
+  end
+
   bind -M insert -m default jj backward-char force-repaint
 end
