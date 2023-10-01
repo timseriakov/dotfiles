fac706380e12fd554b7939f72b326f82edf74fc0
Tim Seriakov <t.seriakov@gmail.com>
2023-10-01 14:47:25 +0300

    add something

--- fish/config.fish
+++ fish/config.fish
@@ -33,7 +33,6 @@ set --universal --export fzf_preview_file_cmd 'bat --style changes --theme Nord 
 set --universal --export fzf_fd_opts --hidden --exclude=.git
 set --universal --export FZF_DEFAULT_OPTS --color fg:#5E81AC,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#2E3440,hl+:#A3BE8C --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker='*'
 set -gx BAT_THEME "Nord"
-set -gx BAT_THEME "Nord"
 
 # pnpm
 set -gx PNPM_HOME "/Users/tim/Library/pnpm"
