eb69ddcb67ac3f993d88924b51879b445d015ca3
Timofey.Seryakov <timofey.seryakov@life.com.by>
2024-02-17 19:58:06 +0300

    add something

--- fish/functions/ch.fish
+++
@@ -1,4 +0,0 @@
-function ch --wraps=cheat --wraps=chafa --description 'alias ch chafa'
-  chafa $argv
-        
-end
---
+++ fish/functions/nid.fish
@@ -0,0 +1,4 @@
+function nid --wraps='npm install -D' --description 'alias nid npm install -D'
+  npm install -D $argv
+        
+end
