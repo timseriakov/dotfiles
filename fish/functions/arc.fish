63f6cc51e806fc4b87cda3721f1a6f14665118b8
Timofey.Seryakov <timofey.seryakov@life.com.by>
2024-03-11 17:29:04 +0300

    add something

---
+++ fish/functions/arc.fish
@@ -0,0 +1,4 @@
+function arc --wraps='lvim ~/.config/alacritty/alacritty.toml' --description 'alias arc lvim ~/.config/alacritty/alacritty.toml'
+  lvim ~/.config/alacritty/alacritty.toml $argv
+        
+end
