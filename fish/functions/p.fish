4a1295fa37c953f2e4d2a31deb43cfb3771028a8
Timofey.Seryakov <timofey.seryakov@life.com.by>
2024-04-20 22:13:34 +0300

    add something

--- fish/functions/p.fish
+++ fish/functions/p.fish
@@ -1,4 +1,4 @@
-function P --wraps='fx package.json' --description 'alias P fx package.json'
-  fx package.json $argv
+function P --wraps='fx package.json' --wraps='jless package.json' --description 'alias P jless package.json'
+  jless package.json $argv
         
 end
