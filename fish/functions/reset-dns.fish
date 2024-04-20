199d12de793459ed3a4d9fdcd6764eead0a72aea
Timofey.Seryakov <timofey.seryakov@life.com.by>
2024-04-20 22:09:48 +0300

    add something

--- fish/functions/reset-dns.fish
+++
@@ -1,4 +0,0 @@
-function reset-dns --wraps='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder' --description 'alias reset-dns sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
-  sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder $argv
-        
-end
