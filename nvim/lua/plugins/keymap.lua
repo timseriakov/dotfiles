794f37aded928ddcc65bb9ef585835f550ed0898
Timofey.Seryakov <timofey.seryakov@life.com.by>
2024-09-06 23:06:15 +0300

    add something

---
+++ nvim/lua/plugins/keymap.lua
@@ -0,0 +1,20 @@
+local opts = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap("n", "x", '"_x', opts)
+vim.api.nvim_set_keymap("n", "X", '"_X', opts)
+
+vim.api.nvim_set_keymap("n", "c", '"_c', opts)
+vim.api.nvim_set_keymap("n", "C", '"_C', opts)
+vim.api.nvim_set_keymap("n", "cc", '"_cc', opts)
+
+vim.api.nvim_set_keymap("n", "d", '"_d', opts)
+vim.api.nvim_set_keymap("n", "D", '"_D', opts)
+vim.api.nvim_set_keymap("n", "dd", '"_dd', opts)
+
+vim.api.nvim_set_keymap("v", "x", '"_x', opts)
+vim.api.nvim_set_keymap("v", "d", '"_d', opts)
+
+vim.api.nvim_set_keymap("n", "m", '"*c', opts)
+vim.api.nvim_set_keymap("n", "mm", '"*cc', opts)
+vim.api.nvim_set_keymap("n", "M", '"*C', opts)
+vim.api.nvim_set_keymap("v", "m", '"*c', opts)
