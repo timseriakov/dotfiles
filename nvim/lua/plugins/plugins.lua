ffcfb38d9a4ea19c73db5f1a795974200e706c61
Timofey.Seryakov <timofey.seryakov@life.com.by>
2024-10-03 16:44:55 +0300

    add something

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -22,49 +22,4 @@ return {
 			},
 		},
 	},
-	-- {
-	--   "AlexvZyl/nordic.nvim",
-	--   opts = {
-	--     bold_keywords = false,
-	--     -- Enable italic comments.
-	--     italic_comments = true,
-	--     -- Enable general editor background transparency.
-	--     transparent_bg = true,
-	--     -- Enable brighter float border.
-	--     bright_border = true,
-	--     -- Reduce the overall amount of blue in the theme (diverges from base Nord).
-	--     reduced_blue = false,
-	--     -- Swap the dark background with the normal one.
-	--     swap_backgrounds = false,
-	--     -- Override the styling of any highlight group.
-	--     override = {},
-	--     -- Cursorline options.  Also includes visual/selection.
-	--     cursorline = {
-	--       -- Bold font in cursorline.
-	--       bold = false,
-	--       -- Bold cursorline number.
-	--       bold_number = true,
-	--       -- Available styles: 'dark', 'light'.
-	--       theme = "dark",
-	--       -- Blending the cursorline bg with the buffer bg.
-	--       blend = 0.7,
-	--     },
-	--     noice = {
-	--       -- Available styles: `classic`, `flat`.
-	--       style = "flat",
-	--     },
-	--     telescope = {
-	--       -- Available styles: `classic`, `flat`.
-	--       style = "flat",
-	--     },
-	--     leap = {
-	--       -- Dims the backdrop when using leap.
-	--       dim_backdrop = false,
-	--     },
-	--     ts_context = {
-	--       -- Enables dark background for treesitter-context window
-	--       dark_background = true,
-	--     },
-	--   },
-	-- },
 }
