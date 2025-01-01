8382083f7fa4155737f74bfc36c3bc8fccd512c1
Timofey.Seryakov <t.seriakov@gmail.com>
2025-01-01 23:07:04 +0300

    add something

--- fish/functions/nrc.fish
+++ fish/functions/nrc.fish
@@ -1,4 +1,4 @@
-function nrc --wraps='v ~/dev/dotfiles/nvim' --wraps='v ~/dev/dotfiles/nvim/lua/plugins' --description 'alias nrc v ~/dev/dotfiles/nvim/lua/plugins'
-  v ~/dev/dotfiles/nvim/lua/plugins $argv
+function nrc --wraps='v ~/dev/dotfiles/nvim' --wraps='v ~/dev/dotfiles/nvim/lua/plugins' --wraps='v ~/dev/dotfiles/nvim/lua' --description 'alias nrc v ~/dev/dotfiles/nvim/lua'
+  v ~/dev/dotfiles/nvim/lua $argv
         
 end
---
+++ nvim/lua/plugins/cursor.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = {
+			stiffness = 0.9,
+			trailing_stiffness = 0.5,
+			trailing_exponent = 777,
+			legacy_computing_symbols_support = true,
+			hide_target_hack = true,
+			gamma = 1,
+		},
+	},
+}
