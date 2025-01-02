1bf023dbae737a172aca0d194f9d50d36ddd337b
Timofey.Seryakov <t.seriakov@gmail.com>
2025-01-02 14:06:10 +0300

    add something

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,7 +7,7 @@ vim.opt.termguicolors = true
 
 -- -- for neovide
 if vim.g.neovide then
-	vim.o.guifont = "ShureTechMono Nerd Font:h17" -- text below applies for VimScript
+	vim.o.guifont = "ShureTechMono Nerd Font:h18"
 
 	vim.g.neovide_fullscreen = true
 
@@ -37,7 +37,21 @@ if vim.g.neovide then
 	vim.g.neovide_hide_mouse_when_typing = true
 	vim.g.neovide_floating_opacity = 1
 
-	-- vim.g.neovide_cursor_vfx_particle_curl = 0.1
-	-- vim.g.neovide_cursor_vfx_particle_lifetime = 3.2
-	-- vim.g.neovide_cursor_vfx_particle_speed = 20.0
+	vim.g.neovide_cursor_vfx_particle_curl = 0.1
+	vim.g.neovide_cursor_vfx_particle_lifetime = 3.2
+	vim.g.neovide_cursor_vfx_particle_speed = 20.0
+
+	-- Allow clipboard copy-paste
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+	vim.keymap.set("t", "<D-v>", [[<C-\><C-N>"+P]]) -- Paste terminal mode
+
+	-- command mapping
+	vim.keymap.set({ "i", "n" }, "<D-a>", "<Esc>ggVG") -- select all
+	vim.keymap.set("n", "<D-z>", "u") -- undo
+
+	-- vim.keymap.set("x", "<D-x>", '"+dm0i<Esc>`0') -- cut (include insert hack to fix whichkey issue #518)
 end
