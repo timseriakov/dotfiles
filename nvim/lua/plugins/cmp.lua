0292a371f9ee34f328bc6616c770ddb352378964
Timofey.Seryakov <t.seriakov@gmail.com>
2025-01-24 15:37:42 +0300

    add something

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"hrsh7th/nvim-cmp",
+		optional = true,
+		dependencies = { "Exafunction/codeium.nvim" },
+		opts = function(_, opts)
+			table.insert(opts.sources, 1, {
+				name = "codeium",
+				group_index = 1,
+				priority = 100,
+			})
+		end,
+	},
+}
