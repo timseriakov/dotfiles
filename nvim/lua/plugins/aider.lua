e7946260ad027ef5234f6502c3c21f5800324d43
Tim Seriakov <t.seriakov@gmail.com>
2025-03-28 17:01:53 +0300

    add something

--- nvim/lua/plugins/aider.lua
+++ nvim/lua/plugins/aider.lua
@@ -1,30 +1,6 @@
 return {
 	"GeorgesAlkhouri/nvim-aider",
-	cmd = {
-		"AiderTerminalToggle",
-		"AiderHealth",
-	},
-	arg = {
-		"--no-auto-commits",
-		"--pretty",
-		"--stream",
-		"--watch-files",
-		"--code-theme nord",
-		"--chat-language RU",
-		"--dark-mode",
-	},
-	theme = {
-		user_input_color = "#88C0D0", -- Arctic Water
-		tool_output_color = "#81A1C1", -- Frost
-		tool_error_color = "#BF616A", -- Aurora (Red)
-		tool_warning_color = "#EBCB8B", -- Aurora (Yellow)
-		assistant_output_color = "#B48EAD", -- Aurora (Purple)
-
-		completion_menu_color = "#E5E9F0", -- Snow Storm (Lightest)
-		completion_menu_bg_color = "#3B4252", -- Polar Night (Dark)
-		completion_menu_current_color = "#ECEFF4", -- Snow Storm (White)
-		completion_menu_current_bg_color = "#4C566A", -- Polar Night (Lighter)
-	},
+	cmd = { "AiderTerminalToggle", "AiderHealth" },
 	keys = {
 		{ "<leader>a/", "<cmd>AiderTerminalToggle<cr>", desc = "Open Aider" },
 		{ "<leader>as", "<cmd>AiderTerminalSend<cr>", desc = "Send to Aider", mode = { "n", "v" } },
@@ -33,13 +9,34 @@ return {
 		{ "<leader>a+", "<cmd>AiderQuickAddFile<cr>", desc = "Add File to Aider" },
 		{ "<leader>a-", "<cmd>AiderQuickDropFile<cr>", desc = "Drop File from Aider" },
 		{ "<leader>ar", "<cmd>AiderQuickReadOnlyFile<cr>", desc = "Add File as Read-Only" },
-		-- Example nvim-tree.lua integration if needed
+		-- nvim-tree integration
 		{ "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
 		{ "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
 	},
 	dependencies = {
 		"folke/snacks.nvim",
-		--- The below dependencies are optional
 		"nvim-tree/nvim-tree.lua",
 	},
+	config = function()
+		require("nvim_aider").setup({
+			aider_cmd = "aider",
+			args = {
+				"--no-auto-commits",
+				"--pretty",
+				"--stream",
+				"--watch-files",
+			},
+			theme = {
+				user_input_color = "#88C0D0", -- Arctic Water
+				tool_output_color = "#81A1C1", -- Frost
+				tool_error_color = "#BF616A", -- Aurora (Red)
+				tool_warning_color = "#EBCB8B", -- Aurora (Yellow)
+				assistant_output_color = "#B48EAD", -- Aurora (Purple)
+				completion_menu_color = "#E5E9F0", -- Snow Storm (Lightest)
+				completion_menu_bg_color = "#3B4252", -- Polar Night (Dark)
+				completion_menu_current_color = "#ECEFF4", -- Snow Storm (White)
+				completion_menu_current_bg_color = "#4C566A", -- Polar Night (Lighter)
+			},
+		})
+	end,
 }
