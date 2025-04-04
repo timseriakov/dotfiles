9d3b5c001c03d96dd1711700f388a5f2e28170b8
Timofey.Seryakov <t.seriakov@gmail.com>
2025-04-04 21:59:01 +0300

    add something

---
+++ fish/functions/a-claude-3.7-sonnet.fish
@@ -0,0 +1,4 @@
+function a-claude-3.7-sonnet --wraps='aider --model openrouter/anthropic/claude-3.7-sonnet -c /Users/tim/.aider.conf.yml' --description 'alias a-claude-3.7-sonnet aider --model openrouter/anthropic/claude-3.7-sonnet -c /Users/tim/.aider.conf.yml'
+  aider --model openrouter/anthropic/claude-3.7-sonnet -c /Users/tim/.aider.conf.yml $argv
+        
+end
---
+++ fish/functions/a-openai-gpt-4o.fish
@@ -0,0 +1,4 @@
+function a-openai-gpt-4o --wraps='aider --model gpt-4o -c /Users/tim/.aider.conf.yml' --description 'alias a-openai-gpt-4o aider --model gpt-4o -c /Users/tim/.aider.conf.yml'
+  aider --model gpt-4o -c /Users/tim/.aider.conf.yml $argv
+        
+end
