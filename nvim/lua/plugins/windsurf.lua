852264dda355acba369514790f7ed1348e1cb8ed
Timofey.Seryakov <t.seriakov@gmail.com>
2025-04-05 20:38:30 +0300

    add something

---
+++ nvim/lua/plugins/windsurf.lua
@@ -0,0 +1,24 @@
+return {
+	"Exafunction/codeium.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		require("codeium").setup({
+			virtual_text = {
+				enabled = true,
+				key_bindings = {
+					accept = "<Tab>",
+					next = "<M-l>",
+					prev = "<M-h>",
+				},
+			},
+		})
+
+		-- обновление lualine
+		require("codeium.virtual_text").set_statusbar_refresh(function()
+			require("lualine").refresh()
+		end)
+	end,
+}
