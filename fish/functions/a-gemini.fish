d01feb2cb83031d8b3ad5eb465a8b50131fa853b
Timofey.Seryakov <t.seriakov@gmail.com>
2025-04-06 18:40:37 +0300

    add something

---
+++ fish/functions/a-gemini.fish
@@ -0,0 +1,4 @@
+function a-gemini --wraps='aider --model gemini -c /Users/tim/.aider.conf.yml' --description 'alias a-gemini aider --model gemini -c /Users/tim/.aider.conf.yml'
+  aider --model gemini -c /Users/tim/.aider.conf.yml $argv
+        
+end
