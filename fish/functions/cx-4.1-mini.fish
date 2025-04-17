7fe843a3f0c01291f0e9af2353cd57e305c75f7a
Timofey.Seryakov <t.seriakov@gmail.com>
2025-04-17 10:46:36 +0300

    add something

---
+++ fish/functions/cx-4.1-mini.fish
@@ -0,0 +1,4 @@
+function cx-4.1-mini --wraps='codex -m gpt-4.1-mini' --description 'alias cx-4.1-mini codex -m gpt-4.1-mini'
+  codex -m gpt-4.1-mini $argv
+        
+end
---
+++ fish/functions/cx-4.1-nano.fish
@@ -0,0 +1,4 @@
+function cx-4.1-nano --wraps='codex -m gpt-4.1-nano' --description 'alias cx-4.1-nano codex -m gpt-4.1-nano'
+  codex -m gpt-4.1-nano $argv
+        
+end
