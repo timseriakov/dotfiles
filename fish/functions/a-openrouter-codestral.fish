28c21b9c3ae284a76265bc9ba0d878d00fed9beb
Timofey.Seryakov <t.seriakov@gmail.com>
2025-04-19 17:38:51 +0300

    add something

---
+++ fish/functions/a-openrouter-codestral.fish
@@ -0,0 +1,4 @@
+function a-openrouter-codestral --wraps='aider --model openrouter/mistralai/codestral-2501 -c /Users/tim/.aider.conf.yml' --description 'alias a-openrouter-codestral aider --model openrouter/mistralai/codestral-2501 -c /Users/tim/.aider.conf.yml'
+  aider --model openrouter/mistralai/codestral-2501 -c /Users/tim/.aider.conf.yml $argv
+        
+end
