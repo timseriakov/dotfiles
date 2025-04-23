6535261b3212a4feaac303790c0d7e1bfe4a80ce
Timofey.Seryakov <t.seriakov@gmail.com>
2025-04-23 11:23:49 +0300

    add something

---
+++ fish/functions/j.fish_
@@ -0,0 +1,4 @@
+function j --wraps=joshuto --description 'alias j joshuto'
+  joshuto $argv
+        
+end
--- nvim/lua/plugins/spamguard.lua
+++ nvim/lua/plugins/spamguard.lua
@@ -1,14 +1,3 @@
 return {
 	"timseriakov/spamguard.nvim",
-	config = function()
-		require("spamguard").setup({
-			keys = {
-				j = { threshold = 9, suggestion = "use s or f instead of spamming jjjj 😎" },
-				k = { threshold = 9, suggestion = "try 10k instead of spamming kkkk 😎" },
-				h = { threshold = 9, suggestion = "use 10h or b / ge 😎" },
-				l = { threshold = 9, suggestion = "try w or e — it's faster! 😎" },
-				w = { threshold = 9, suggestion = "use s or f — more precise and quicker! 😎" },
-			},
-		})
-	end,
 }
