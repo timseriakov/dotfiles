8ade7e3d67e40325d6d24ef9b5a6138f68ff11cc
Timofey.Seryakov <t.seriakov@gmail.com>
2025-05-03 21:29:44 +0300

    feat(nvim): add nvim-spider plugin configuration

---
+++ nvim/lua/plugins/nvim-sniper.lua
@@ -0,0 +1,31 @@
+return {
+  "chrisgrieser/nvim-spider",
+  event = "VeryLazy",
+  config = function()
+    require("spider").setup({
+      skipInsignificantPunctuation = true,
+      consistentOperatorPending = true,
+    })
+
+    vim.schedule(function()
+      vim.keymap.set(
+        { "n", "o", "x" },
+        "w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        { noremap = true, silent = true }
+      )
+      vim.keymap.set(
+        { "n", "o", "x" },
+        "e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        { noremap = true, silent = true }
+      )
+      vim.keymap.set(
+        { "n", "o", "x" },
+        "b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        { noremap = true, silent = true }
+      )
+    end)
+  end,
+}
