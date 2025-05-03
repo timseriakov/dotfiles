249469c192aa6a24b59248ce5e67c09d60d7bb4b
Timofey.Seryakov <t.seriakov@gmail.com>
2025-05-03 21:04:43 +0300

    refactor(spamguard): enhance spamguard setup with key thresholds

--- nvim/lua/plugins/spamguard.lua
+++ nvim/lua/plugins/spamguard.lua
@@ -1,3 +1,19 @@
 return {
-	"timseriakov/spamguard.nvim",
+  "timseriakov/spamguard.nvim",
+  event = "VeryLazy",
+  config = function()
+    vim.schedule(function()
+      local sg = require("spamguard")
+      sg.setup({
+        keys = {
+          j = { threshold = 10, suggestion = "use s or f instead of spamming jjjj 😎" },
+          k = { threshold = 10, suggestion = "try 10k instead of spamming kkkk 😎" },
+          h = { threshold = 10, suggestion = "use 10h or b / ge 😎" },
+          l = { threshold = 10, suggestion = "try w or e — it's faster! 😎" },
+          w = { threshold = 7, suggestion = "use s or f — more precise and quicker! 😎" },
+        },
+      })
+      sg.enable()
+    end)
+  end,
 }
