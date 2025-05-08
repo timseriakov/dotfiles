67e5596565e6fe223afba5917b20c4dc40ba45a7
Timofey.Seryakov <t.seriakov@gmail.com>
2025-05-08 09:01:34 +0300

    feat(find-and-replace): rename nvim-rip-substitute to find-and-replace

--- nvim/lua/plugins/find-and-replace.lua
+++ nvim/lua/plugins/find-and-replace.lua
@@ -4,7 +4,7 @@ return {
   opts = {},
   keys = {
     {
-      "<leader>rr",
+      "<leader>br",
       function()
         require("rip-substitute").sub()
       end,
--- nvim/lua/plugins/opener.lua
+++ nvim/lua/plugins/opener.lua
@@ -1,30 +1,30 @@
 return {
-	{
-		"willthbill/opener.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim" },
-		config = function()
-			require("opener").setup({
-				pre_open = function(new_dir)
-					print("Opening " .. new_dir)
-				end,
-				post_open = function(new_dir)
-					print("Opened " .. new_dir)
-				end,
-			})
+  {
+    "willthbill/opener.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("opener").setup({
+        pre_open = function(new_dir)
+          print("Opening " .. new_dir)
+        end,
+        post_open = function(new_dir)
+          print("Opened " .. new_dir)
+        end,
+      })
 
-			require("telescope").setup({
-				extensions = {
-					opener = {
-						hidden = false,
-						root_dir = "~/dev",
-						respect_gitignore = true,
-					},
-				},
-			})
+      require("telescope").setup({
+        extensions = {
+          opener = {
+            hidden = false,
+            root_dir = "~/dev",
+            respect_gitignore = true,
+          },
+        },
+      })
 
-			require("telescope").load_extension("opener")
+      require("telescope").load_extension("opener")
 
-			vim.api.nvim_set_keymap("n", "<leader>o", ":Telescope opener<cr>", { noremap = true, silent = true })
-		end,
-	},
+      vim.api.nvim_set_keymap("n", "<leader>fo", ":Telescope opener<cr>", { noremap = true, silent = true })
+    end,
+  },
 }
