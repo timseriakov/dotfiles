5125b92f4ce0d0d5903c19ebfe76c85727e1d258
Timofey.Seryakov <t.seriakov@gmail.com>
2025-05-17 00:03:36 +0300

    feat(ai-commit): update keymap for selecting files in AI commit

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,7 +1,13 @@
 return {
-	{ "akinsho/bufferline.nvim", enabled = false },
-	{
-		"github/copilot.vim",
-		enabled = false,
-	},
+  { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "github/copilot.vim",
+    enabled = false,
+  },
+  {
+    "LazyVim/LazyVim",
+    keys = {
+      { "<leader>gs", false },
+    },
+  },
 }
