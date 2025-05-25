d8ae28826e7d175b1c4c11b7bad1ee5ea96330cb
Timofey.Seryakov <t.seriakov@gmail.com>
2025-05-25 20:44:20 +0300

    feat(tiny-code-action): update backend to use difftastic for improved diffing

--- nvim/lua/plugins/tiny-code-action.lua
+++ nvim/lua/plugins/tiny-code-action.lua
@@ -1,71 +1,82 @@
 return {
-  "rachartier/tiny-code-action.nvim",
-  event = "LspAttach",
-  dependencies = {
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-telescope/telescope.nvim" },
-    -- Альтернатива: snacks.nvim или fzf-lua по вкусу
-    {
-      "folke/snacks.nvim",
-      opts = {
-        terminal = {},
-      },
-    },
-  },
-  opts = {
-    backend = "vim", -- или "delta", если установлен delta
-    picker = {
-      "telescope",
-      opts = {
-        layout_strategy = "vertical",
-        layout_config = {
-          width = 0.7,
-          height = 0.9,
-          preview_cutoff = 1,
-          preview_height = function(_, _, max_lines)
-            local h = math.floor(max_lines * 0.5)
-            return math.max(h, 10)
-          end,
+  {
+    "rachartier/tiny-code-action.nvim",
+    event = "LspAttach",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+      {
+        "folke/snacks.nvim",
+        opts = {
+          terminal = {},
         },
       },
     },
-    backend_opts = {
-      delta = {
-        header_lines_to_remove = 4,
-        args = { "--line-numbers" },
-      },
-      difftastic = {
-        header_lines_to_remove = 1,
-        args = {
-          "--color=always",
-          "--display=inline",
-          "--syntax-highlight=on",
+    opts = {
+      backend = "difftastic",
+      picker = {
+        "telescope",
+        opts = {
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.9,
+            height = 0.9,
+            preview_cutoff = 1,
+            preview_height = function(_, _, max_lines)
+              local h = math.floor(max_lines * 0.5)
+              return math.max(h, 10)
+            end,
+          },
         },
       },
-      diffsofancy = {
-        header_lines_to_remove = 4,
+      backend_opts = {
+        delta = {
+          header_lines_to_remove = 4,
+          args = { "--line-numbers" },
+        },
+        difftastic = {
+          header_lines_to_remove = 1,
+          args = {
+            "--color=always",
+            "--display=inline",
+            "--syntax-highlight=on",
+          },
+        },
+        diffsofancy = {
+          header_lines_to_remove = 4,
+        },
+      },
+      signs = {
+        quickfix = { "󰁨", { link = "DiagnosticInfo" } },
+        others = { "?", { link = "DiagnosticWarning" } },
+        refactor = { "", { link = "DiagnosticWarning" } },
+        ["refactor.move"] = { "󰪹", { link = "DiagnosticInfo" } },
+        ["refactor.extract"] = { "", { link = "DiagnosticError" } },
+        ["source.organizeImports"] = { "", { link = "DiagnosticWarning" } },
+        ["source.fixAll"] = { "", { link = "DiagnosticError" } },
+        ["source"] = { "", { link = "DiagnosticError" } },
+        ["rename"] = { "󰑕", { link = "DiagnosticWarning" } },
+        ["codeAction"] = { "", { link = "DiagnosticError" } },
       },
     },
-    signs = {
-      quickfix = { "󰁨", { link = "DiagnosticInfo" } },
-      others = { "?", { link = "DiagnosticWarning" } },
-      refactor = { "", { link = "DiagnosticWarning" } },
-      ["refactor.move"] = { "󰪹", { link = "DiagnosticInfo" } },
-      ["refactor.extract"] = { "", { link = "DiagnosticError" } },
-      ["source.organizeImports"] = { "", { link = "DiagnosticWarning" } },
-      ["source.fixAll"] = { "", { link = "DiagnosticError" } },
-      ["source"] = { "", { link = "DiagnosticError" } },
-      ["rename"] = { "󰑕", { link = "DiagnosticWarning" } },
-      ["codeAction"] = { "", { link = "DiagnosticError" } },
-    },
   },
-  keys = {
-    {
-      "<leader>cj",
-      function()
-        require("tiny-code-action").code_action()
-      end,
-      desc = "Code Action (Tiny)",
-    },
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+      -- Отключаем дефолтный биндинг <leader>ca
+      keys[#keys + 1] = { "<leader>ca", false }
+
+      keys[#keys + 1] = {
+        "<leader>ca",
+        function()
+          require("tiny-code-action").code_action()
+        end,
+        desc = "Code Action",
+        mode = { "n", "v" },
+        has = "codeAction",
+      }
+    end,
   },
 }
