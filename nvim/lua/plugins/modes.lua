b5ec4da740a4c5449f24a9b5b52c68d3525dc039
Timofey.Seryakov <t.seriakov@gmail.com>
2025-05-27 22:51:56 +0300

    chore(modes): implement modes.nvim configuration for better UX in Neovim

---
+++ nvim/lua/plugins/modes.lua
@@ -0,0 +1,20 @@
+return {
+  "timseriakov/modes.nvim",
+  event = "BufReadPre",
+  config = function()
+    require("modes").setup({
+      colors = {
+        copy = "#ebcb8b",
+        delete = "#bf616a",
+        insert = "#88c0d0",
+        visual = "#A3BE8C",
+      },
+      line_opacity = 0.08,
+      set_cursor = true,
+      set_cursorline = true,
+      set_number = true,
+      set_signcolumn = true,
+      ignore = { "!neo-tree", "TelescopePrompt", "!minifiles" },
+    })
+  end,
+}
