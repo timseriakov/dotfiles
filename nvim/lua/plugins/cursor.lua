244f9dc5dc962b7ad14ba9051328b87024fd4743
Timofey.Seryakov <t.seriakov@gmail.com>
2025-05-29 14:35:41 +0300

    refactor(nvim): reorganize neo-tree mappings for better usability

--- nvim/lua/plugins/cursor.lua
+++ nvim/lua/plugins/cursor.lua
@@ -1,13 +1,14 @@
 return {
-	{
-		"sphamba/smear-cursor.nvim",
-		opts = {
-			stiffness = 0.9,
-			trailing_stiffness = 0.5,
-			trailing_exponent = 777,
-			legacy_computing_symbols_support = true,
-			hide_target_hack = true,
-			gamma = 1,
-		},
-	},
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      smear_between_buffers = false,
+      legacy_computing_symbols_support = true,
+      hide_target_hack = true,
+      never_draw_over_target = true,
+
+      stiffness = 0.5,
+      trailing_stiffness = 0.49,
+    },
+  },
 }
