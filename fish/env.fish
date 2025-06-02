dae890eb106990af714790a0d671f4d9139c941a
Timofey.Seryakov <t.seriakov@gmail.com>
2025-06-02 22:44:41 +0300

    chore(fish): enhance fzf bindings for better usability

--- fish/env.fish
+++ fish/env.fish
@@ -1,9 +1,17 @@
 set -gx NEOVIDE_TITLE_HIDDEN 1
 set -x HOMEBREW_NO_ENV_HINTS 1
 
-set --universal --export fzf_preview_file_cmd 'bat --style changes --decorations never --theme Nord --color=always'
+# fzf
 set --universal --export fzf_fd_opts --hidden --exclude=.git
-set --universal --export FZF_DEFAULT_OPTS --color fg:#5E81AC,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#2E3440,hl+:#A3BE8C --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker='*'
+set --universal --export fzf_preview_file_cmd 'bat --style=plain --color=always --theme Nord'
+set -gx fzf_directory_opts --color=border:#81A1C1
+set -gx fzf_git_log_opts --color=border:#81A1C1
+set -gx fzf_git_status_opts --color=border:#81A1C1
+set -gx fzf_history_opts --color=border:#81A1C1
+set -gx fzf_processes_opts --color=border:#81A1C1
+set -gx fzf_variables_opts --color=border:#81A1C1
+
+# bat
 set -gx BAT_THEME Nord
 
 # posting
