85f4a01350730d9e4469b802af1bea0944cad010
Timofey.Seryakov <t.seriakov@gmail.com>
2025-06-03 15:08:13 +0300

    style(env): update variable export syntax for consistency

--- fish/env.fish
+++ fish/env.fish
@@ -1,9 +1,9 @@
 set -gx NEOVIDE_TITLE_HIDDEN 1
-set -x HOMEBREW_NO_ENV_HINTS 1
+set -gx HOMEBREW_NO_ENV_HINTS 1
 
 # fzf
-set --universal --export fzf_fd_opts --hidden --exclude=.git
-set --universal --export fzf_preview_file_cmd 'bat --style=plain --color=always --theme Nord'
+set -gx fzf_fd_opts --hidden --exclude=.git
+set -gx fzf_preview_file_cmd 'bat --style=plain --color=always --theme Nord'
 set -gx fzf_directory_opts --color=border:#81A1C1
 set -gx fzf_git_log_opts --color=border:#81A1C1
 set -gx fzf_git_status_opts --color=border:#81A1C1
