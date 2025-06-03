5f967510e1d548999efd5d3bbd856f25106c7718
Timofey.Seryakov <t.seriakov@gmail.com>
2025-06-03 15:06:01 +0300

    feat(fish): enhance session startup command for improved user experience

--- fish/functions/_fzf_search_git_status.fish
+++ fish/functions/_fzf_search_git_status.fish
@@ -7,7 +7,7 @@ function _fzf_search_git_status --description "Search the output of git status. 
             git -c color.status=always status --short |
             _fzf_wrapper --ansi \
                 --multi \
-                --prompt="Search Git Status> " \
+                --prompt="Git Status> " \
                 --query=(commandline --current-token) \
                 --preview='_fzf_preview_changed_file {}' \
                 --nth="2.." \
