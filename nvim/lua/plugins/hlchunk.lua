644f8874fa026017c34fd658f101ab45644e3b84
Timofey.Seryakov <t.seriakov@gmail.com>
2025-06-04 23:35:27 +0300

    feat(hlchunk): add hlchunk.nvim plugin for enhanced code chunk highlighting

---
+++ nvim/lua/plugins/hlchunk.lua
@@ -0,0 +1,40 @@
+return {
+  "shellRaining/hlchunk.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("hlchunk").setup({
+      chunk = {
+        enable = true,
+        use_treesitter = true,
+        style = {
+          { fg = "#88C0D0" },
+        },
+        chars = {
+          right_arrow = "─",
+        },
+        duration = 100,
+        delay = 100,
+      },
+      indent = {
+        enable = true,
+        chars = { "│" },
+        style = {
+          { fg = "#4C566A" },
+        },
+      },
+      line_num = {
+        enable = true,
+        style = {
+          { fg = "#81A1C1" },
+        },
+      },
+      blank = {
+        enable = true,
+        chars = { "⋅" },
+        style = {
+          { fg = "#3B4252" },
+        },
+      },
+    })
+  end,
+}
