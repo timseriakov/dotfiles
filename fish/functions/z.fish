2836ee93914b81109ac8e11b62e9807d24b93c17
Timofei.Seriakov <t.seriakov@gmail.com>
2025-06-22 20:57:17 +0300

    add something

--- fish/functions/z.fish
+++ fish/functions/z.fish
@@ -2,12 +2,12 @@ function z --wraps=__zoxide_z --description 'z with smart eza listing'
     set -g __prev_dir (pwd)
     __zoxide_z $argv; or return
 
-    set lines (eza --all --long --icons --color=always | wc -l)
+    set lines (eza --all --long --icons --group-directories-first --color=always | wc -l)
     set height (tput lines)
 
     if test $lines -gt $height
         eza --all --icons --group-directories-first
     else
-        eza --all --long --icons
+        eza --all --long --icons --group-directories-first
     end
 end
