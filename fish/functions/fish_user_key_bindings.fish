f677f80c903c0be657467628428ea478ca1a53c7
Timofei.Seriakov <t.seriakov@gmail.com>
2025-06-24 14:20:02 +0300

    feat(kitty): add ctrl+enter mapping for fish accept-autosuggestion

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,12 +1,13 @@
 function fish_user_key_bindings
-  fish_vi_key_bindings
-  for mode in insert default visual
-      bind -M $mode \ck 'history --merge ; up-or-search'
-      bind -M $mode \cj 'history --merge ; down-or-search' 
-      bind -M $mode \ch accept-autosuggestion execute
-      bind -M $mode \cl accept-autosuggestion
+    fish_vi_key_bindings
+    for mode in insert default visual
+        bind -M $mode \ck 'history --merge ; up-or-search'
+        bind -M $mode \cj 'history --merge ; down-or-search'
+        bind -M $mode \"\r\" accept-autosuggestion execute
+        bind -M $mode \ch accept-autosuggestion execute
+        bind -M $mode \cl accept-autosuggestion
 
-  end
+    end
 
-  bind -M insert -m default jj backward-char force-repaint
+    bind -M insert -m default jj backward-char force-repaint
 end
