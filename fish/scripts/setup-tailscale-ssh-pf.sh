85543cf99f028c37e52b86b6a9ee78903136df58
Timofei.Seriakov <t.seriakov@gmail.com>
2025-06-25 10:50:34 +0300

    chore(setup-tailscale-ssh-pf): update pf rules for Tailscale interface

--- fish/scripts/setup-tailscale-ssh-pf.sh
+++ fish/scripts/setup-tailscale-ssh-pf.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 set -e
 
 echo "󰋼  Looking for Tailscale IP and interface..."
-sleep 5
+sleep 2
 
 TS_INTERFACE=$(ifconfig | awk '
 /^[a-z]/ { iface=$1 }
@@ -11,9 +10,7 @@ TS_INTERFACE=$(ifconfig | awk '
 ')
 
 if [ -z "$TS_INTERFACE" ]; then
-  echo "󰅙  Could not detect utun interface with 100.x.x.x address"
-  echo "  --- Debug ifconfig dump ---"
-  ifconfig
+  echo "󰅙  Could not detect Tailscale interface"
   exit 1
 fi
 
@@ -26,20 +23,18 @@ echo "󰩈  Writing config to $PF_FILE..."
 sudo tee "$PF_FILE" >/dev/null <<EOF
 set skip on lo0
 scrub in all
-block all
+
+pass in quick on $TS_INTERFACE proto tcp from 100.0.0.0/8 to any port 22 keep state
+block drop in quick proto tcp from any to any port = 22
 
 pass out all keep state
-
-pass in on $TS_INTERFACE proto tcp from 100.0.0.0/8 to any port 22 keep state
 EOF
 
-echo "󰈼  pf.conf written. Contents:"
+echo "󰈼  pf.conf written:"
 cat "$PF_FILE"
 
 echo "󱓻  Reloading pf with new rules..."
-sudo pfctl -f "$PF_FILE" 2>/dev/null || true
-sudo pfctl -e 2>/dev/null || true
+sudo pfctl -f "$PF_FILE" || true
+sudo pfctl -e || true
 
 echo "󰄬  pf enabled and rules applied"
-
-exit 0
