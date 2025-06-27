e54705d7624e8dca6dcdd01af44f445071e05d5c
Timofei.Seriakov <t.seriakov@gmail.com>
2025-06-27 21:00:59 +0300

    feat(env): reorganize environment variables and add new configurations

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,3 @@
-# if status is-interactive
-#     if not set -q TMUX; and not set -q IN_NEOVIDE; and not set -q NO_TMUX
-#         exec tmux
-#     else if set -q TASKWARRIOR
-#         exec /usr/local/bin/taskwarrior-tui
-#         exec /opt/homebrew/bin/taskwarrior-tui
-#     end
-# end
-#
-
 if status is-interactive
     if not set -q TMUX; and not set -q IN_NEOVIDE; and not set -q NO_TMUX; and not set -q NVIM
         exec tmux
@@ -18,43 +8,12 @@ if set -q NVIM
     set -gx TMPDIR /tmp
 end
 
-if type -q ngrok
-    ngrok completion | source
-end
-
-set -g -x fish_greeting Welcome
-
 starship init fish | source
 
-fish_vi_key_bindings
-set fish_key_bindings fish_user_key_bindings
-
-set -gx ATUIN_NOBIND true
-atuin init fish | source
-
-# bind to ctrl-r in normal and insert mode, add any other bindings you want here too
-bind \cr _atuin_search
-bind -M insert \cr _atuin_search
-
-pyenv init - | source
-
-set -gx fish_command_timeout 8000
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
+# OrbStack tools
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 set -Ux fish_user_paths $HOME/.orbstack/bin $fish_user_paths
 
-zoxide init fish | source
-functions -e z
-source ~/.config/fish/functions/z.fish
-
-# secrets.fish
 if test -f ~/dev/dotfiles/fish/secrets.fish
     source ~/dev/dotfiles/fish/secrets.fish
-end
-
-# Load custom envs
-if test -f ~/dev/dotfiles/fish/env.fish
-    source ~/dev/dotfiles/fish/env.fish
 end
--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,15 +1,15 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
+
     for mode in insert default visual
-        bind -M $mode \ck 'history --merge ; up-or-search'
-        bind -M $mode \cj 'history --merge ; down-or-search'
+        bind -M $mode \ck 'history --merge; up-or-search'
+        bind -M $mode \cj 'history --merge; down-or-search'
         bind -M $mode \"\r\" accept-autosuggestion execute
         bind -M $mode \ch 'cd ..; commandline -f repaint'
         bind -M $mode \cb 'cd -; commandline -f repaint'
         bind -M $mode \cl accept-autosuggestion
-        # bind \cb prevd
-        # bind \cf nextd
     end
 
-    bind -M insert -m default jj backward-char force-repaint
+    # jj to escape insert → normal with correct cursor mode
+    bind -M insert -m default jj 'set -g fish_bind_mode default; commandline -f backward-char repaint'
 end
