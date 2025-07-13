36f5bef62493ceefb22c2f01d5057c122cc5f74a
Timofei.Seriakov <t.seriakov@gmail.com>
2025-07-13 13:57:42 +0300

    feat(fish-functions): add mtrx and mtrx-cyan functions for cmatrix

---
+++ fish/functions/mtrx-cyan.fish
@@ -0,0 +1,4 @@
+function mtrx-cyan --wraps='cmatrix -C cyan' --wraps='cmatrix -C cyan -s' --description 'alias mtrx-cyan cmatrix -C cyan -s'
+  cmatrix -C cyan -s $argv
+        
+end
---
+++ fish/functions/mtrx.fish
@@ -0,0 +1,4 @@
+function mtrx --wraps=cmatrix --wraps='cmatrix -C blue' --wraps='cmatrix -C blue -s' --description 'alias mtrx cmatrix -C blue -s'
+  cmatrix -C blue -s $argv
+        
+end
