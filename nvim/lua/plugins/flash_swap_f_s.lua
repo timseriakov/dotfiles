5a17ed5c126d5693c0d5276cef5a309e9bbff491
Timofei.Seriakov <t.seriakov@gmail.com>
2025-07-14 10:45:53 +0300

    feat(plugins): add flash.nvim for enhanced jump functionality

---
+++ nvim/lua/plugins/flash_swap_f_s.lua
@@ -0,0 +1,77 @@
+return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+      char = {
+        enabled = false,
+      },
+    },
+    config = function(_, opts)
+      require("flash").setup(opts)
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          for _, mode in ipairs({ "n", "x", "o" }) do
+            for _, key in ipairs({ "f", "F", "t", "T", "s" }) do
+              pcall(vim.keymap.del, mode, key)
+            end
+          end
+        end,
+      })
+
+      vim.schedule(function()
+        local flash = require("flash")
+
+        vim.keymap.set({ "n", "x", "o" }, "f", flash.jump, {
+          desc = "Flash jump (default)",
+          noremap = true,
+          silent = true,
+        })
+
+        vim.keymap.set({ "n", "x", "o" }, "s", flash.treesitter, {
+          desc = "Flash treesitter",
+          noremap = true,
+          silent = true,
+        })
+
+        vim.keymap.set({ "n", "x", "o" }, "F", function()
+          flash.jump({
+            label = { pos = "top" },
+            pattern = "^",
+            search = { mode = "search" },
+          })
+        end, {
+          desc = "Flash line labels (top)",
+          noremap = true,
+          silent = true,
+        })
+      end)
+    end,
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    keys = {
+      {
+        "S",
+        mode = "x",
+        function()
+          require("Comment.api").toggle.linewise(vim.fn.visualmode())
+        end,
+        desc = "Toggle comment (visual)",
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    opts = {
+      move = {
+        enable = false,
+        set_jumps = false,
+      },
+    },
+  },
+}
