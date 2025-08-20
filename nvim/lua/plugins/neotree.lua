e3a529ba8137128de9b9d78d6fc4f546dcceabcb
Timofei.Seriakov <t.seriakov@gmail.com>
2025-08-20 19:54:32 +0300

    fix(telescope): include hidden files in fd search

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  init = function()
+    -- Overridden to prevent auto-opening
+  end,
   opts = function(_, opts)
     opts.filesystem = opts.filesystem or {}
     opts.filesystem.window = opts.filesystem.window or {}
