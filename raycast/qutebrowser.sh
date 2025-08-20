fcf85ae71e7a593171e0795b86da859432554e7b
Timofei.Seriakov <t.seriakov@gmail.com>
2025-08-20 19:43:11 +0300

    chore(deps): update dependency versions in lazy-lock.json

--- raycast/qutebrowser.sh
+++ raycast/qutebrowser.sh
@@ -4,7 +4,7 @@
 # @raycast.mode silent
 # @raycast.package Browsers
 # @raycast.icon 🚀
-# @raycast.argument1 { "type": "text", "placeholder": "URL (optional)", "optional": true }
+## @raycast.argument1 { "type": "text", "placeholder": "URL (optional)", "optional": true }
 
 # Ensure Homebrew in PATH
 export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
@@ -12,10 +12,10 @@ export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PAT
 # Set Qt plugins path so Cocoa/WebEngine work
 export QT_PLUGIN_PATH="$(brew --prefix qt@6)/plugins"
 
-URL="$1"
+#URL="$1"
 
-if [ -n "$URL" ]; then
-  exec /opt/homebrew/bin/qutebrowser "$URL"
-else
-  exec /opt/homebrew/bin/qutebrowser
-fi
+#if [ -n "$URL" ]; then
+#   exec /opt/homebrew/bin/qutebrowser "$URL"
+# else
+exec /opt/homebrew/bin/qutebrowser
+# fi
