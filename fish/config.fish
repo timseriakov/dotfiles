d25b7b56dde4d2b2167e865bbd4e673147be390d
Timofei.Seriakov <t.seriakov@gmail.com>
2025-08-27 12:14:19 +0300

    feat(shell): add support for React Native terminal settings

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,13 @@ end
 
 starship init fish | source
 
-
 if test -f ~/dev/dotfiles/fish/secrets.fish
     source ~/dev/dotfiles/fish/secrets.fish
 end
+
+# React Native Terminal Settings
+set -gx REACT_TERMINAL kitty
+set -gx RCT_METRO_PORT 8081
+
+# Added by `rbenv init` on Mon Aug 25 15:17:59 +03 2025
+status --is-interactive; and rbenv init - --no-rehash fish | source
---
+++ fish/functions/cl.fish
@@ -0,0 +1,4 @@
+function cl --wraps=claude-code --wraps=claude --description 'alias cl claude'
+  claude $argv
+        
+end
