7f3bb43b6b87edf9f9e5f0b261d3610625242af3
Timofei.Seriakov <t.seriakov@gmail.com>
2025-09-10 22:42:42 +0300

    fix(fish): wrong binding

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -4,7 +4,7 @@ function fish_user_key_bindings
     for mode in insert default visual
         bind -M $mode \ck 'history --merge; up-or-search'
         bind -M $mode \cj 'history --merge; down-or-search'
-        bind -M $mode \r accept-autosuggestion execute
+        bind -M $mode \"\r\" accept-autosuggestion execute
         bind -M $mode \ch 'cd ..; commandline -f repaint'
         bind -M $mode \cb 'cd -; commandline -f repaint'
         bind -M $mode \cl accept-autosuggestion
