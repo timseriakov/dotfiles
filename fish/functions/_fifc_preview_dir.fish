ca9195ab097d1e5df691f0e863d879b2644b8130
Timofei.Seriakov <t.seriakov@gmail.com>
2025-12-05 12:49:51 +0300

    Use eza for fzf directory previews

--- fish/functions/_fifc_preview_dir.fish
+++ fish/functions/_fifc_preview_dir.fish
@@ -1,6 +1,6 @@
 function _fifc_preview_dir -d "List content of the selected directory"
-    if type -q exa
-        exa --color=always $fifc_exa_opts $fifc_candidate
+    if type -q eza
+        eza --color=always --icons --group-directories-first $fifc_exa_opts $fifc_candidate
     else
         ls --color=always $fifc_ls_opts $fifc_candidate
     end
