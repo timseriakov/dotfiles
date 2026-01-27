48d1b601e69bc91dea6a7ad1ec220b7bf694befb
Timofei.Seriakov <t.seriakov@gmail.com>
2026-01-27 23:02:44 +0300

    feat: install DreamMaoMao/searchjump.yazi with Nord theme

--- yazi/init.lua
+++ yazi/init.lua
@@ -2,12 +2,16 @@ require("starship"):setup()
 require("git"):setup()
 require("no-status"):setup()
 require("close-and-restore-tab")
-require("easyjump"):setup({
-	icon_fg = "#A3BE8C", -- color for hint labels "#88C1D0"
-	first_key_fg = "#ffffff", -- color for first char of double-key hints #A3BE8C
-	first_keys = "asdfgercwtvxbq", -- 14 keys
-	second_keys = "yuiophjklnm", -- 11 keys
+require("searchjump"):setup({
+	opt_unmatch_fg = "#4C566A",
+	opt_match_str_fg = "#2E3440",
+	opt_match_str_bg = "#A3BE8C",
+	opt_lable_fg = "#ECEFF4",
+	opt_lable_bg = "#D08770",
+	opt_only_current = false, -- only search the current window
+	opt_search_patterns = {}, -- demo:{"%.e%d+","s%d+e%d+"}
 })
+
 
 if os.getenv("NVIM") then
 	require("toggle-pane"):entry("min-preview")
