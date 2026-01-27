4617451df8f9aa62403f91e187b03d90dd3d0882
Timofei.Seriakov <t.seriakov@gmail.com>
2026-01-27 23:08:44 +0300

    feat: apply refined Nord colors for searchjump.yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -5,9 +5,9 @@ require("close-and-restore-tab")
 require("searchjump"):setup({
 	opt_unmatch_fg = "#4C566A",
 	opt_match_str_fg = "#2E3440",
-	opt_match_str_bg = "#A3BE8C",
-	opt_lable_fg = "#ECEFF4",
-	opt_lable_bg = "#D08770",
+	opt_match_str_bg = "#88C0D0",
+	opt_lable_fg = "#2E3440",
+	opt_lable_bg = "#EBCB8B",
 	opt_only_current = false, -- only search the current window
 	opt_search_patterns = {}, -- demo:{"%.e%d+","s%d+e%d+"}
 })
