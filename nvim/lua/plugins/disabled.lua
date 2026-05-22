df985ed992c01da6d5946615a85fbe35e8cf3b3b
Tim.Seriakov <t.seriakov@gmail.com>
2026-05-22 12:53:59 +0300

    fix(nvim): restore plugin compatibility

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -21,4 +21,29 @@ return {
   -- If you had copilot-cmp before, keep it disabled to avoid dual engines
   { "zbirenbaum/copilot-cmp", enabled = false },
   -- Keep blink.cmp as your sole completion engine (LazyVim extra usually enables it)
+
+  -- Compatibility pins for plugins whose current main branches moved past NVIM 0.11
+  -- or changed dependencies faster than LazyVim extras.
+  { "stevearc/aerial.nvim", branch = "nvim-0.11" },
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "lewis6991/async.nvim",
+    },
+    config = function(_, opts)
+      require("refactoring").setup(opts)
+    end,
+    keys = {
+      {
+        "<leader>rs",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = { "n", "x" },
+        desc = "Refactor",
+      },
+    },
+  },
 }
