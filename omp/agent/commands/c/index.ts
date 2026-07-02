97ec1eb4c754e401844a76dfb05d855595a8efa8
Tim.Seriakov <t.seriakov@gmail.com>
2026-07-03 00:31:38 +0300

    fixup: english description for /c

--- omp/agent/commands/c/index.ts
+++ omp/agent/commands/c/index.ts
@@ -8,7 +8,7 @@ import type { HookCommandContext } from "@oh-my-pi/pi-coding-agent/extensibility
 export default function commitCommand(_api: CustomCommandAPI): CustomCommand {
   return {
     name: "c",
-    description: "Закоммитить текущие изменения",
+    description: "Commit current changes",
     execute(_args: string[], _ctx: HookCommandContext): string | undefined {
       return "Закоммить текущие изменения. Пиши сообщение на английском, conventional commit. Ответь кратко, что и как закоммитил, на русском.";
     },
