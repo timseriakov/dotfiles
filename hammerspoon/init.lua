e173f53dec57e56e1944becf0c0da2c39778893a
Tim.Seriakov <t.seriakov@gmail.com>
2026-07-17 19:37:37 +0300

    Move UT2004 double jump to Karabiner

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,35 +6,6 @@ qutebrowserTemporarySplit.init()
 local tmuxSeshLauncher = require("tmux_sesh_launcher")
 tmuxSeshLauncher.init()
 
-ut2004JumpTap = hs.eventtap.new({ hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp }, function(event)
-	local app = hs.application.frontmostApplication()
-	local isUT2004 = app ~= nil and (app:bundleID() == "com.oldunreal.UT2004" or app:name() == "UT2004")
-	if not isUT2004 or event:getKeyCode() ~= hs.keycodes.map.space then
-		return false
-	end
-
-	local flags = event:getFlags()
-	if flags.cmd or flags.ctrl or flags.alt then
-		return false
-	end
-
-	if event:getType() == hs.eventtap.event.types.keyUp then
-		return true
-	end
-
-	local isRepeat = event:getProperty(hs.eventtap.event.properties.keyboardEventAutorepeat) == 1
-	if not isRepeat then
-		hs.eventtap.keyStroke({}, "h", 0)
-		hs.timer.doAfter(0.34, function()
-			local frontmost = hs.application.frontmostApplication()
-			local gameActive = frontmost ~= nil and (frontmost:bundleID() == "com.oldunreal.UT2004" or frontmost:name() == "UT2004")
-			if gameActive then
-				hs.eventtap.keyStroke({}, "h", 0)
-			end
-		end)
-	end
-	return true
-end):start()
 -- Auto-reload Hammerspoon config on change
 local function reloadConfig(files)
 	local doReload = false
