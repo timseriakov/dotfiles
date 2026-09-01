de500990db59c6a18d25309d2c8f7f1551f8febb
Tim.Seriakov <t.seriakov@gmail.com>
2026-09-01 17:12:31 +0300

    Move qutebrowser statusbar to top

--- qutebrowser/modules/settings.py
+++ qutebrowser/modules/settings.py
@@ -15,7 +15,7 @@ c.window.hide_decoration = True
 # Layout
 c.scrolling.bar = "when-searching"
 c.statusbar.show = "always"
-c.statusbar.position = "bottom"
+c.statusbar.position = "top"
 c.tabs.show = "always"  # multiple # never
 c.tabs.last_close = "blank"
 c.tabs.position = "top"
