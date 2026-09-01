140d9c56efbfe829f06232a64903ab376da39be8
Tim.Seriakov <t.seriakov@gmail.com>
2026-09-01 17:21:03 +0300

    Place qutebrowser statusbar below tabs

--- qutebrowser/modules/qutebrowser_patches.py
+++ qutebrowser/modules/qutebrowser_patches.py
@@ -41,9 +41,12 @@ miscmodels.session = _wide_session_completion
 # in a conventional browser. Use an event filter because PyQt/SIP does not
 # reliably redispatch C++ virtual mouse events after monkey-patching a method.
 from qutebrowser.commands import runners
-from qutebrowser.mainwindow import mainwindow
-from qutebrowser.qt.core import QEvent, QObject, Qt
-from qutebrowser.qt.widgets import QApplication
+from qutebrowser.config import config
+from qutebrowser.mainwindow import mainwindow, tabwidget
+from qutebrowser.mainwindow.statusbar import bar
+from qutebrowser.qt.core import QEvent, QObject, QPoint, QRect, Qt, QTimer
+from qutebrowser.qt.widgets import QApplication, QSizePolicy, QStyle, QTabWidget
+from qutebrowser.utils import qtutils
 
 
 _orig_mainwindow_init = getattr(
@@ -80,6 +83,35 @@ class _StatusUrlClickFilter(QObject):
         return True
 
 
+def _align_status_url(status):
+    status.url.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+    status.url.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+    hbox = getattr(status, "_hbox", None)
+    if hbox is not None:
+        url_index = hbox.indexOf(status.url)
+        for i in range(hbox.count()):
+            hbox.setStretch(i, 1 if i == url_index else 0)
+        hbox.invalidate()
+        hbox.activate()
+    status.url.updateGeometry()
+    status.updateGeometry()
+
+
+_orig_statusbar_draw_widgets = getattr(
+    bar.StatusBar,
+    "_dotfiles_orig_draw_widgets",
+    bar.StatusBar._draw_widgets,
+)
+bar.StatusBar._dotfiles_orig_draw_widgets = _orig_statusbar_draw_widgets
+
+
+def _draw_widgets_with_right_aligned_url(self):
+    _orig_statusbar_draw_widgets(self)
+    _align_status_url(self)
+
+
+bar.StatusBar._draw_widgets = _draw_widgets_with_right_aligned_url
+
 def _mainwindow_init_with_url_click(self, *args, **kwargs):
     _orig_mainwindow_init(self, *args, **kwargs)
     app = QApplication.instance()
@@ -90,6 +122,171 @@ def _mainwindow_init_with_url_click(self, *args, **kwargs):
     if app is not None:
         app.installEventFilter(click_filter)
     self.status.url._dotfiles_click_filter = click_filter
+    _align_status_url(self.status)
+
+
+def _below_tab_status_enabled(window):
+    return (
+        config.val.statusbar.position == "top"
+        and window.tabbed_browser.widget.tabPosition() == QTabWidget.TabPosition.North
+    )
+
+
+class _BelowTabStatusStyle(tabwidget.TabBarStyle):
+    def subElementRect(self, element, option, widget=None):
+        rect = super().subElementRect(element, option, widget)
+        if widget is not getattr(self, "_dotfiles_tabwidget", None) or element not in (
+            QStyle.SubElement.SE_TabWidgetTabPane,
+            QStyle.SubElement.SE_TabWidgetTabContents,
+        ):
+            return rect
+
+        status = getattr(widget, "_dotfiles_below_tab_status", None)
+        if status is None or status.parent() is not widget or not status.isVisible():
+            return rect
+
+        tabbar = widget.tabBar()
+        tab_bottom = tabbar.geometry().bottom() + 1 if tabbar.isVisible() else 0
+        wanted_top = tab_bottom + status.sizeHint().height()
+        if rect.top() < wanted_top:
+            rect.setTop(wanted_top)
+        return rect
+
+
+def _ensure_below_tab_style(tabwidget_):
+    if not isinstance(tabwidget_.style(), _BelowTabStatusStyle):
+        style = _BelowTabStatusStyle()
+        style._dotfiles_tabwidget = tabwidget_
+        tabwidget_._dotfiles_below_tab_style = style
+        tabwidget_.setStyle(style)
+
+
+def _place_status_below_tabs(window):
+    tabwidget = window.tabbed_browser.widget
+    if not _below_tab_status_enabled(window) or window.status.parent() is not tabwidget:
+        return
+
+    tabbar = tabwidget.tabBar()
+    y = tabbar.geometry().bottom() + 1 if tabbar.isVisible() else 0
+    window.status.setGeometry(0, y, tabwidget.width(), window.status.sizeHint().height())
+    window.status.raise_()
+    tabwidget.setDocumentMode(tabwidget.documentMode())
+
+
+class _BelowTabStatusRelayoutFilter(QObject):
+    def __init__(self, window, parent=None):
+        super().__init__(parent)
+        self._window = window
+        self._pending = False
+
+    def eventFilter(self, watched, event):
+        if event.type() in (QEvent.Type.Resize, QEvent.Type.Show, QEvent.Type.Hide):
+            if not self._pending:
+                self._pending = True
+                QTimer.singleShot(0, self._relayout)
+        return False
+
+    def _relayout(self):
+        self._pending = False
+        _place_status_below_tabs(self._window)
+
+
+def _install_below_tab_relayout_filter(window):
+    tabwidget = window.tabbed_browser.widget
+    old_filter = getattr(tabwidget, "_dotfiles_below_tab_filter", None)
+    if old_filter is not None:
+        tabwidget.removeEventFilter(old_filter)
+        tabwidget.tabBar().removeEventFilter(old_filter)
+
+    relayout_filter = _BelowTabStatusRelayoutFilter(window, tabwidget)
+    tabwidget.installEventFilter(relayout_filter)
+    tabwidget.tabBar().installEventFilter(relayout_filter)
+    tabwidget._dotfiles_below_tab_filter = relayout_filter
+
+
+_orig_add_widgets = getattr(
+    mainwindow.MainWindow,
+    "_dotfiles_orig_add_widgets",
+    mainwindow.MainWindow._add_widgets,
+)
+mainwindow.MainWindow._dotfiles_orig_add_widgets = _orig_add_widgets
+
+
+def _add_widgets_with_below_tab_status(self):
+    if not _below_tab_status_enabled(self):
+        if self.status.parent() is not self:
+            self.status.setParent(self)
+        _orig_add_widgets(self)
+        return
+
+    _orig_add_widgets(self)
+    self._vbox.removeWidget(self.status)
+    tabwidget = self.tabbed_browser.widget
+    self.status.setParent(tabwidget)
+    tabwidget._dotfiles_below_tab_status = self.status
+    _ensure_below_tab_style(tabwidget)
+    self.status.show()
+    _install_below_tab_relayout_filter(self)
+    _place_status_below_tabs(self)
+
+
+mainwindow.MainWindow._add_widgets = _add_widgets_with_below_tab_status
+
+
+_orig_update_overlay_geometry = getattr(
+    mainwindow.MainWindow,
+    "_dotfiles_orig_update_overlay_geometry",
+    mainwindow.MainWindow._update_overlay_geometry,
+)
+mainwindow.MainWindow._dotfiles_orig_update_overlay_geometry = _orig_update_overlay_geometry
+
+
+def _update_overlay_geometry_with_reparented_status(self, widget, centered, padding):
+    if not (
+        config.val.statusbar.position == "top"
+        and self.status.parent() is self.tabbed_browser.widget
+        and self.tabbed_browser.widget.tabPosition() == QTabWidget.TabPosition.North
+    ):
+        _orig_update_overlay_geometry(self, widget, centered, padding)
+        return
+
+    if not widget.isVisible():
+        return
+
+    if widget.sizePolicy().horizontalPolicy() == QSizePolicy.Policy.Expanding:
+        width = self.width() - 2 * padding
+        height = widget.heightForWidth(width) if widget.hasHeightForWidth() else widget.sizeHint().height()
+        left = padding
+    else:
+        size_hint = widget.sizeHint()
+        width = min(size_hint.width(), self.width() - 2 * padding)
+        height = size_hint.height()
+        left = (self.width() - width) // 2 if centered else 0
+
+    top = self.status.mapTo(self, self.status.rect().bottomLeft()).y() + 1
+    bottom = qtutils.check_overflow(top + height, "int", fatal=False)
+    rect = QRect(
+        QPoint(left, top),
+        QPoint(left + width, min(self.height() - 20, bottom)),
+    )
+    if rect.isValid():
+        widget.setGeometry(rect)
+
+
+mainwindow.MainWindow._update_overlay_geometry = _update_overlay_geometry_with_reparented_status
 
 
 mainwindow.MainWindow.__init__ = _mainwindow_init_with_url_click
+
+
+def _align_existing_status_urls():
+    app = QApplication.instance()
+    if app is None:
+        return
+    for widget in app.allWidgets():
+        if getattr(widget, "url", None) is not None and getattr(widget, "_hbox", None) is not None:
+            _align_status_url(widget)
+
+
+_align_existing_status_urls()
+QTimer.singleShot(0, _align_existing_status_urls)
